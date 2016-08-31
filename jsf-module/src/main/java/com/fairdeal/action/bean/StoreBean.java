5360599463f9fa0062ecc30d83506edb69e9d203
Puneet.behl <puneet739@gmail.com>
2016-09-01 00:43:01 +0530

    Signed-off-by: Puneet.behl <puneet739@gmail.com>

--- jsf-module/src/main/java/com/fairdeal/action/bean/StoreBean.java
+++ jsf-module/src/main/java/com/fairdeal/action/bean/StoreBean.java
@@ -6,5 +6,9 @@ import com.property.player.model.mongo.Store;
 
 public class StoreBean extends Store implements Serializable{
 
+	public String getCordinates(){
+		String result = getLocation().getPoint().getX()+","+getLocation().getPoint().getY();
+		return result;
+	}
 	
 }
