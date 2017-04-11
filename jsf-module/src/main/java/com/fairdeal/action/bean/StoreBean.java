1181ed11d5f7702982412813f09fd45e24b4bb51
Puneet.behl <puneet739@gmail.com>
2017-04-11 20:22:58 +0530

    Null pointer check if no location is present.

--- jsf-module/src/main/java/com/fairdeal/action/bean/StoreBean.java
+++ jsf-module/src/main/java/com/fairdeal/action/bean/StoreBean.java
@@ -2,12 +2,18 @@ package com.fairdeal.action.bean;
 
 import java.io.Serializable;
 
+import com.fairdeal.utility.LoggerUtil;
 import com.property.player.model.mongo.Store;
 
 public class StoreBean extends Store implements Serializable{
 
 	public String getCordinates(){
-		String result = getLocation().getPoint().getX()+","+getLocation().getPoint().getY();
+		String result="0,0";
+		try{
+			result = getLocation().getPoint().getX()+","+getLocation().getPoint().getY();
+		}catch(NullPointerException ne){
+			LoggerUtil.error("No Location co-ordinates exist for Store: "+this);
+		}
 		return result;
 	}
 	
