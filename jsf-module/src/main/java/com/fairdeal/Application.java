fd57fc9e1551a5bf90da8db185be59be5cbef3d7
Puneet Behl <bpuneet@athenahealth.com>
2019-03-02 09:44:17 +0530

    Making spring boot application

--- jsf-module/src/main/java/com/fairdeal/Application.java
+++ jsf-module/src/main/java/com/fairdeal/Application.java
@@ -1,12 +1,21 @@
 package com.fairdeal;
 
+import org.apache.log4j.chainsaw.Main;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@EnableAutoConfiguration
-public class Application {
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(new Class[] { Main.class, Initializer.class });
 	}
 }
